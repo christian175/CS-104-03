d3c7a90277ad3566e57e9bba7e9f72b73eb4df06
christian175 <55259741+christian175@users.noreply.github.com>
2019-09-24 16:32:35 -0400

    Update guessing game.py

--- guessing game.py
+++ guessing game.py
@@ -1,42 +1,37 @@
 import random
 
+#these are the rules of the game
 ComputerNumber = random.randint(1,1000000)
 GameOver = False
 guessNumber = 0
 lowRange = 1
 highRange = 1000000
-
+print("guess a number between one and a million")
 #this is the game loop
 while not GameOver:
-    print("Enter an integer")
-    print(highRange, lowRange)
-    playerGuess = input()
+    print(highRange,lowRange)
+    playerGuess = input("enter an integer:")
     x = int(playerGuess)
     guessNumber = guessNumber + 1
     print (guessNumber)
     if x == ComputerNumber:
-        print ("you win")
+        print ("you guessed the correct number")
+        print ("you win!")
         GameOver = True
     elif guessNumber == 20:
-        print("you lose")
+        print("you lose:(")
         print ("the correct number is", ComputerNumber)
         GameOver = True
     elif x > highRange:
         print ("out of range")
         guessNumber = guessNumber - 1
-        print (highRange, lowRange)
     elif x < lowRange:
         print ("out of range")
-        print (highRange, lowRange)
     elif x > ComputerNumber:
         print ("sorry too high")
         print ("adjusting range")
         highRange = x
-        print(highRange, lowRange)
     elif x < ComputerNumber:
         print ("sorry too low")
         print ("adjusting range")
         lowRange = x
-        print (highRange, lowRange)
-        
-        
